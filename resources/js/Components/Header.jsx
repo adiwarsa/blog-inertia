a44044e2cb515d44932f5ff0ddd6c24495758f04
adiwarsa <adiwarsa03@gmail.com>
2023-09-10 20:36:26 +0800

    adding Context for Theme

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -1,13 +1,15 @@
 import Container from '@/Components/Container.jsx';
+import { useTheme } from '@/Context/ThemeContext';
 
 export default function Header({ title, subtitle }) {
+    const { theme } = useTheme();
     return (
-        <div className="border-b border-t border-gray-800 bg-gray-900 py-10 sm:py-20">
+        <div className="border-b border-t border-gray-800 bg-slate-300 dark:bg-gray-900 py-10 sm:py-20">
             <Container>
                 <div className="max-w-xl">
                     <div>
-                        <h2 className="mb-2 text-3xl font-bold tracking-tight text-white">{title}</h2>
-                        <p className="text-lg leading-8 text-gray-400">{subtitle}</p>
+                    <h2 className={`mb-2 text-3xl font-bold tracking-tight ${theme === 'light' ? 'text-black' : 'text-white'}`}>{title}</h2>
+                        <p className={`text-lg leading-8 ${theme === 'light' ? 'text-black' : 'text-gray-400'}`}>{subtitle}</p>
                     </div>
                 </div>
             </Container>
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,7 @@ import '../css/app.css';
 import { hydrateRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ThemeProvider } from './Context/ThemeContext';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -11,7 +12,9 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*')),
     setup({ el, App, props }) {
-        hydrateRoot(el, <App {...props} />)
+        hydrateRoot(el, <ThemeProvider>
+            <App {...props} />
+        </ThemeProvider>)
     },
     progress: {
         color: '#4B5563',
